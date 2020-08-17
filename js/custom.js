3ba95378e2a36062ed3362191fc05882db35b508
Lee Slimming <leeslimming@gmail.com>
2020-08-17 10:52:54 +0400

    Update UI

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,14 @@
-$(function() {
-    $('#search').on('keyup', function() {
-        var pattern = $(this).val();
-        $('.searchable-container .items').hide();
-        $('.searchable-container .items').filter(function() {
-            return $(this).text().match(new RegExp(pattern, 'i'));
-        }).show();
-    });
-});
-$(document).ready(function() {
-	$(".megamenu").on("click", function(e) {
-		e.stopPropagation();
-	});
-});
+// $(function() {
+//     $('#search').on('keyup', function() {
+//         var pattern = $(this).val();
+//         $('.searchable-container .items').hide();
+//         $('.searchable-container .items').filter(function() {
+//             return $(this).text().match(new RegExp(pattern, 'i'));
+//         }).show();
+//     });
+// });
+// $(document).ready(function() {
+// 	$(".megamenu").on("click", function(e) {
+// 		e.stopPropagation();
+// 	});
+// });
